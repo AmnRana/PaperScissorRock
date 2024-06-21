aa854f150f440177073d6186f0a4593e3a27e2c6
AmnRana <168154612+AmnRana@users.noreply.github.com>
2024-06-21 04:48:22 +0000

    added you loose condition

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('Hellow world')
+// Creating a simple Javascript Paper Scissor Rock Game 
+// --------------------***--------------------------------
+
+
+let GameAsseset = ['Paper', 'Scissor', 'Rock'];
+
+let ComputerGeneratedNumber= Math.floor(Math.random()*3);
+
+
+let UserInputValue = 'Scissor'
+let IndexOfUserinput = GameAsseset.indexOf(UserInputValue)
+console.log(IndexOfUserinput)
+console.log(ComputerGeneratedNumber)
+
+if (IndexOfUserinput == 0){
+    if(ComputerGeneratedNumber==1){
+        console.log('you loose')
+    }
+}else if(IndexOfUserinput == 1){
+    if(ComputerGeneratedNumber==2){
+        console.log('you loose')
+    }
+}
+else{
+    if(ComputerGeneratedNumber==0){
+        console.log('you loose')
+    }
+}
+
+
+
+

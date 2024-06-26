b772afafa6559ce0052433362ad69882cfa43737
AmnRana <168154612+AmnRana@users.noreply.github.com>
2024-06-26 04:35:05 +0000

    working on js

--- main.js
+++ main.js
@@ -1,51 +1,82 @@
 // Creating a simple Javascript Paper Scissor Rock Game 
 // --------------------***--------------------------------
-
+console.log("hello")
 
 let GameAsseset = ['Paper', 'Scissor', 'Rock'];
+let UserInputValue
+let htmlPaper = document.querySelector(".Paper")
+let htmlScissor = document.querySelector(".Scissor")
+let htmlRock = document.querySelector(".Rock")
+let ComputerGeneratedNumber;
 
-let ComputerGeneratedNumber= Math.floor(Math.random()*3);
+htmlPaper.addEventListener('click',()=>{
+    UserInputValue=0
+    ComputerGeneratedNumber= Math.floor(Math.random()*3)
+    console.log(UserInputValue)
+    console.log(ComputerGeneratedNumber)
+    playGame()
+})
+
+htmlScissor.addEventListener('click',()=>{
+    UserInputValue = 1
+    ComputerGeneratedNumber= Math.floor(Math.random()*3)
+    console.log(UserInputValue)
+    console.log(ComputerGeneratedNumber)
+    playGame()
+})
+
+htmlRock.addEventListener('click',()=>{
+    UserInputValue=2
+    ComputerGeneratedNumber= Math.floor(Math.random()*3)
+    console.log(UserInputValue)
+    console.log(ComputerGeneratedNumber)
+    playGame()
+})
 
 
-let UserInputValue = 'Paper'
-let IndexOfUserinput = GameAsseset.indexOf(UserInputValue)
-console.log(IndexOfUserinput)
-console.log(ComputerGeneratedNumber)
-
-if (IndexOfUserinput == 0){
-    if(ComputerGeneratedNumber==1){
-        console.log('you loose')
+// let IndexOfUserinput = GameAsseset.indexOf(UserInputValue)
+// console.log(UserInputValue)
+// console.log(ComputerGeneratedNumber)
+function playGame(){
+    if (UserInputValue == 0){
+        if(ComputerGeneratedNumber==1){
+            console.log('you loose')
+        }
+        else if(ComputerGeneratedNumber==2){
+            console.log('you win')
+        }
+        else{
+            console.log('tie')
+        }
     }
-    else if(ComputerGeneratedNumber==2){
-        console.log('you win')
+    
+    else if(UserInputValue == 1){
+        if(ComputerGeneratedNumber==2){
+            console.log('you loose')
+        }
+        else if(ComputerGeneratedNumber==0){
+            console.log('you win')
+        }
+        else{
+            console.log('tie')
+        }
     }
     else{
-        console.log('tie')
+        if(ComputerGeneratedNumber==0){
+            console.log('you loose')
+        }
+        else if(ComputerGeneratedNumber==1){
+            console.log('you win')
+        }
+        else{
+            console.log('tie')
+        }
     }
+    UserInputValue = null
+
 }
 
-else if(IndexOfUserinput == 1){
-    if(ComputerGeneratedNumber==2){
-        console.log('you loose')
-    }
-    else if(ComputerGeneratedNumber==0){
-        console.log('you win')
-    }
-    else{
-        console.log('tie')
-    }
-}
-else{
-    if(ComputerGeneratedNumber==0){
-        console.log('you loose')
-    }
-    else if(ComputerGeneratedNumber==1){
-        console.log('you win')
-    }
-    else{
-        console.log('tie')
-    }
-}
+
 
 
 

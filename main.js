d267968cef91b05c018d0b0ac195ea6b0bcf788f
AmnRana <168154612+AmnRana@users.noreply.github.com>
2024-06-30 05:51:15 +0000

    added mid-container functionality

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Creating a simple Javascript Paper Scissor Rock Game 
 // --------------------***--------------------------------
-console.log("hello")
 
 let GameAsseset = ['Paper', 'Scissor', 'Rock'];
 let UserInputValue
@@ -8,76 +7,85 @@ let htmlPaper = document.querySelector(".Paper")
 let htmlScissor = document.querySelector(".Scissor")
 let htmlRock = document.querySelector(".Rock")
 let ComputerGeneratedSign =document.querySelector(".ComputerGeneratedSign")
+let displayResult=document.querySelector(".displayResult")
+let StartButton=document.querySelector(".start")
 let ComputerGeneratedNumber;
 
 
 htmlPaper.addEventListener('click',()=>{
     UserInputValue=0
-    ComputerGeneratedNumber= Math.floor(Math.random()*3)
-    console.log(UserInputValue)
-    console.log(ComputerGeneratedNumber)
-    playGame()
 })
 
 htmlScissor.addEventListener('click',()=>{
     UserInputValue = 1
-    ComputerGeneratedNumber= Math.floor(Math.random()*3)
-    console.log(UserInputValue)
-    console.log(ComputerGeneratedNumber)
-    playGame()
 })
 
 htmlRock.addEventListener('click',()=>{
     UserInputValue=2
-    ComputerGeneratedNumber= Math.floor(Math.random()*3)
-    console.log(UserInputValue)
-    console.log(ComputerGeneratedNumber)
-    playGame()
 })
 
+StartButton.addEventListener('click', () => playGame())
+
 function playGame(){
-    // ComputerGeneratedNumber= Math.floor(Math.random()*3)
-    if (ComputerGeneratedNumber==0){
+    ComputerGeneratedNumber= Math.floor(Math.random()*3)
+    console.log(ComputerGeneratedNumber)
+    console.log(UserInputValue)
+    if(UserInputValue==undefined && UserInputValue==null ){
+        //do nothing to not add any image
+    }else if (ComputerGeneratedNumber==0){
         ComputerGeneratedSign.style.background="url('./paper.svg')"
+        ComputerGeneratedSign.style.backgroundRepeat="no-repeat"
     }else if(ComputerGeneratedNumber==1){
           ComputerGeneratedSign.style.background="url('./scssior.svg')"
-    }
-    else{
+          ComputerGeneratedSign.style.backgroundRepeat="no-repeat"
+    }else{
         ComputerGeneratedSign.style.background="url('./rock.svg')"
+        ComputerGeneratedSign.style.backgroundRepeat="no-repeat"
     }
 
-    if (UserInputValue == 0){
+    if(UserInputValue==undefined || UserInputValue==null){
+        displayResult.innerHTML="Please select any sign to play"
+    }else if (UserInputValue == 0){
         if(ComputerGeneratedNumber==1){
             console.log('you loose')
+            displayResult.innerHTML="You Lose"
         }
         else if(ComputerGeneratedNumber==2){
             console.log('you win')
+            displayResult.innerHTML="You Win"
         }
         else{
             console.log('tie')
+            displayResult.innerHTML="Tie"
         }
     }
     
     else if(UserInputValue == 1){
         if(ComputerGeneratedNumber==2){
             console.log('you loose')
+            displayResult.innerHTML="You Lose"
         }
         else if(ComputerGeneratedNumber==0){
             console.log('you win')
+            displayResult.innerHTML="You Win"
         }
         else{
             console.log('tie')
+            displayResult.innerHTML="Tie"
         }
     }
     else{
         if(ComputerGeneratedNumber==0){
             console.log('you loose')
+            displayResult.innerHTML="You Lose"
         }
         else if(ComputerGeneratedNumber==1){
             console.log('you win')
+            displayResult.innerHTML="You Win"
         }
         else{
             console.log('tie')
+            displayResult.innerHTML="Tie"
         }
     }
     UserInputValue = null
